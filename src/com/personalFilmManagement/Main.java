8a14473714a2999a888c787dd69b3a9bb0df7a97
user <user@DESKTOP-IBU7IRB>
2017-05-09 12:41:03 +0800

    add simple test

--- src/com/personalFilmManagement/Main.java
+++ src/com/personalFilmManagement/Main.java
@@ -1,32 +1,30 @@
 package com.personalFilmManagement;
 
 import java.util.LinkedList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class Main {
 
 	public static void main(String[] args) {
-		Pattern  msgsPa =Pattern.compile(CompressionScheme.toReguasrExpression());
-		Matcher  msgsMa =msgsPa.matcher("Assassins.Creed.2016.1080p.3D.BluRay.AVC.DTS-HD.MA.7.1");
-		msgsMa.find();
-		
-		//System.out.println(msgsMa.group(0));
-		
-		Movie m=new Movie();
-		m.setAudioCoding(AudioCoding.AAC);
-		//DBoperator.update(m,()->"ENGname='Assassins Creed'");
-		//DBoperator.insert(m);
-		//LinkedList<Movie> res= DBoperator.select(()->"*",()->"",new OrderBy(OrderType.ASC, "ENGname"));
-		//Requestprocesser.autoInsertPath("刺客信条.Assassins.Creed.2016.1080p.3D.BluRay.AVC.DTS-HD.MA.7.1");
-		//LinkedList<Movie> lm;
-		//lm=Requestprocesser.orderBy(new OrderBy(OrderType.ASC,"ENGname"));
-		//lm.forEach(System.out::println);
+		//使用文件夹目录自动添加
+		System.out.println("使用文件目录自动添加");
 		Requestprocesser.autoInstertDirectory("C:\\Users\\user\\Desktop\\sd");
+		//英文名升序输出
+		System.out.println("英文名升序输出");
 		LinkedList<Movie> outSet;
 		outSet=Requestprocesser.orderBy(new OrderBy(OrderType.ASC,"ENGname"));
 		outSet.forEach(System.out::println);
+		//手动修改或更新数据
+		System.out.println("更新colonia数据");
+		String id="Colonia";
+		Requestprocesser.update(id, null, null, null, null, null, null, null, AudioCoding.TRUEHD);
+		//使用英文查询
+		System.out.println("查询Coloin的信息");
+		System.out.println(Requestprocesser.search("Colonia").toString());
+		//使用中文查询
+		System.out.println("查询尊严殖民地的信息");
+		System.out.println(Requestprocesser.search("尊严殖民地").toString());
+
 	}
 
 }
